7f5741c607b733f3fc900daeaf094e987351ed6e
愛朱歌 <asuka.nomura.zzz@gmail.com>
2024-10-20 16:35:53 +0900

    I added useState and useEffect

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 
 const page = () => {
+  const [name, setName] = useState("Hanako");
+  
+  useEffect(() => {
+    // fetch("https://jsonplaceholder.typicode.com/users/1")
+    //   .then((res) => res.json())
+    //   .then((data) => setName(data.name))
+
+
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1")
+      const data = await res.json()
+      setName(data.name)
+    } //declare
+
+    fetchData()  //call
+
+  },[])
+
   return (
     <div>
-      form
+      {name}
+      <form>
+        <input type="text" className="border" onChange={(event) => setName(event.target.value)}/>
+        <button type="submit" className="bg-blue-200">送信</button>
+      </form>
     </div>
   )
 }
