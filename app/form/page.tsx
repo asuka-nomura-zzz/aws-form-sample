bbef89bd5a196cbd3ea418fe84fb76d8809dd617
愛朱歌 <asuka.nomura.zzz@gmail.com>
2024-10-22 02:02:16 +0900

    I connected to supabase

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import React, { useState, useEffect } from 'react'
+import { supabase } from '../lib/createClient'
 
 const page = () => {
   const [name, setName] = useState("Hanako");
+  const [sections, setSections] = useState<any[] | null>([]);
+  const [selectedSection, setSelectedSection] = useState("")
 
   useEffect(() => {
     // fetch("https://jsonplaceholder.typicode.com/users/1")
@@ -11,9 +14,13 @@ const page = () => {
     //   .then((data) => setName(data.name))
 
     const fetchData = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users/1")
-      const data = await res.json()
-      setName(data.name)
+      // const res = await fetch("https://jsonplaceholder.typicode.com/users/1")
+      // const data = await res.json()
+      // setName(data.name)
+      // const { data } = await supabase.from("sections").select();
+      const { data } = await supabase.from("timeslots").select();
+
+      setSections(data)
     } //declare
 
     fetchData()  //call
@@ -22,11 +29,20 @@ const page = () => {
 
   return (
     <div>
-      {name}
+
+      <p>{name}</p>
+      <p>{selectedSection}</p>
+
       <form>
         <input type="text" className="border" onChange={(event) => setName(event.target.value)}/>
+        <select onChange={(event) => setSelectedSection(event.target.value)}>
+          {sections?.map((section)=>(
+            <option key={section.id}>{section.name} : {section.stock}</option> 
+        ))}
+        </select>
         <button type="submit" className="bg-blue-200">送信</button>
       </form>
+
     </div>
   )
 }
