0b76c5c56c223b8d599ab5ade2808d51898013d9
asuka-nomura-zzz <asuka.nomura.zzz@gmail.com>
2024-11-17 18:09:50 +0900

    first commit

--- app/hooks/useAppContext.tsx
+++ app/hooks/useAppContext.tsx
@@ -3,41 +3,33 @@ import { useState, useEffect } from 'react'
 import { createContext, useContext, FC, PropsWithChildren } from 'react'
 import { Timeslot } from '@/app/types/Timeslot';
 import { getTimeslotsFromAws } from '../utils/getTimeslotsFromAws';
-import { Influencer } from '../types/Influencer';
 import { getInfluencersFromAws } from '../utils/getInfluencers';
 import { InfluencerWithId } from '../types/InfluencerWithId';
 
 export const AppContext = createContext<{
-  timeslots: Timeslot[],
   timeslotsFromAws: Timeslot[],
   influencersFromAws: InfluencerWithId[],
 
   //for ISR
   setInfluencers: (influencers: InfluencerWithId[]) => void,
   setTimeslots: (timeslots: Timeslot[]) => void,
-
   refreshTimeslots: () => void,
 }>({
-  timeslots: [],
   timeslotsFromAws: [],
   influencersFromAws: [],
 
   //for ISR
   setInfluencers: () => {},
   setTimeslots: () => {},
-
   refreshTimeslots: () => { }
 });
 
 export const AppWrapper: FC<PropsWithChildren<{
-  timeslots: Timeslot[],
   timeslotsFromAws: Timeslot[]
   influencersFromAws: InfluencerWithId[]
-  // setInfluencers: (influencers: InfluencerWithId[]) => {},
-}>> = ({ timeslots, timeslotsFromAws, influencersFromAws, children }) => {
+}>> = ({ timeslotsFromAws, influencersFromAws, children }) => {
 
   //for ISR
-  const [timeslotsState, setTimeslotsState] = useState<Timeslot[]>(timeslots)
   const [timeslotsFromAwsState, setTimeslotsFromAwsState] = useState<Timeslot[]>(timeslotsFromAws)
   const [influencersFromAwsState, setInfluencersFromAwsState] = useState<InfluencerWithId[]>(influencersFromAws)
 
@@ -67,13 +59,11 @@ export const AppWrapper: FC<PropsWithChildren<{
 
   //for ISR
   const refreshTimeslots = () => {
-    setTimeslotsState(timeslotsState)
     setTimeslotsFromAwsState(timeslotsFromAwsState)
   }
 
   return (
     <AppContext.Provider value={{
-      timeslots: timeslotsState,
       timeslotsFromAws: timeslotsFromAwsState,
       influencersFromAws: influencersFromAwsState,
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,8 @@ import type { Metadata } from "next";
 import { AppWrapper } from "./hooks/useAppContext";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-
-import { getTimeslots } from "./utils/getTimeslots";
 import { getTimeslotsFromAws } from "./utils/getTimeslotsFromAws";
 import { Timeslot } from "./types/Timeslot";
-import { Influencer } from "./types/Influencer";
 import { getInfluencersFromAws } from "./utils/getInfluencers";
 import Link from "next/link";
 import { InfluencerWithId } from "./types/InfluencerWithId";
@@ -20,19 +17,11 @@ export const metadata: Metadata = {
 export const revalidate = 0;
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  const fetchedData = await getTimeslots();
-
-  const timeslots: Timeslot[] = (fetchedData ?? []).map(item => ({
-    id: item.id as number,
-    name: item.name as string,
-    stock: item.stock as number,
-  }));
-
   const timeslotsFromAws: Timeslot[] = await getTimeslotsFromAws()
   const influencersFromAws: InfluencerWithId[] = await getInfluencersFromAws()
 
   return (
-    <AppWrapper timeslots={timeslots} timeslotsFromAws={timeslotsFromAws} influencersFromAws={influencersFromAws} >
+    <AppWrapper timeslotsFromAws={timeslotsFromAws} influencersFromAws={influencersFromAws} >
       <html lang="ja">
         <body className="antialiased">
           <div className="max-w-8xl mx-auto min-h-screen my-10 xl:mx-auto p-6 bg-white">
